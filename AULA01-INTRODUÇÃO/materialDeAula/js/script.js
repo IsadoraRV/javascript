66ad753316f4f42b82590c81b810c5bd83e8c505
vitindls275 <Victorsouzaribeiro20@gmail.com>
2023-11-21 17:47:57 -0300

    atuali

---
+++ AULA01-INTRODUÇÃO/materialDeAula/js/script.js
@@ -0,0 +1,44 @@
+console.log("Olá mundo! :)");
+
+// caixas de texto
+alert(
+    "Olá, este é meu primeiro comando Js!"
+);
+
+confirm("Estou aprendendo Js?");
+
+prompt("Qual é o seu nome?");
+
+// VARIÁVEIS    
+
+//LET escopo local, sobrescrevível
+
+// CONST escopo global, não sobrescrevível
+
+//VAR escopo global, sobrescrevível
+
+var nome = 'Victor';
+
+//CONST 
+const pi = 3.14
+console.log(pi)
+
+// VAR escopo global
+var nome = "Victor"
+
+var idade = prompt("Qual é a sua idade?")
+
+alert('Seja bem vindoo(a)! $(nome)')
+
+//tipos de valores
+//number números
+var real = 25;
+
+//string textos
+
+var texto = "exemplo"
+
+//boolean
+
+
+
